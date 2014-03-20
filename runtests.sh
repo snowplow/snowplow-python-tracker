6eb760cd127d0de1aa51703a1ec7201a835f40ad
Alexander Dean <alex@snowplowanalytics.com>
2014-03-20 16:49:34 +0200

    Removed out of date comment

--- runtests.sh
+++ runtests.sh
@@ -3,10 +3,5 @@
 
 cd $(dirname $0)
 
-# Run any one of the following three commands.
-# Personally I prefer pytest because it has a neat output but travis-ci uses
-# nosetests. The choice of choosing nosetests or pytest should not make a
-# difference to the way tests are run (a test failing under nosetests *will*
-# fail under pytests and the other way round).
-
+# pytest because it has a neat output
 /vagrant/snowplow-python-tracker-environment/bin/python3.3 -m pytest -s
