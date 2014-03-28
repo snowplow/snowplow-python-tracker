b50b035ae43d2b5b4777a029542a6ab9c27b6080
Alex Dean <alex@snowplowanalytics.com>
2014-03-28 19:47:52 +0200

    Fixes for publishing

--- setup.py
+++ setup.py
@@ -23,7 +23,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 import os
 
 version_file_path = os.path.join(
