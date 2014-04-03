00de7fc76165a04d00ecd58ad716e57dc9168e85
Fred Blundun <fred@snowplowanalytics.com>
2014-04-03 12:26:41 +0100

    run-tests.sh now runs tests for Python 3.3 and 2.7 (#42) Verified compatibility with Python 2.7

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,7 @@
 cd $(dirname $0)
 
 # pytest because it has a neat output
-/vagrant/snowplow-python-tracker-environment/bin/python3.3 -m pytest -s
+
+/vagrant/snowplow-python-3.3-tracker-environment/bin/python3.3 -m pytest -s
+
+/vagrant/snowplow-python-2.7-tracker-environment/bin/python2.7 -m pytest -s
