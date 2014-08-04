cb12e6ba313628e56c179cd781841346d6360f3f
Fred Blundun <fred@snowplowanalytics.com>
2014-08-04 14:01:37 +0100

    Converted payload values to strings for POST requests - fixes #100

--- snowplow_tracker/emitters.py
+++ snowplow_tracker/emitters.py
@@ -133,7 +133,10 @@ class Emitter(object):
             :param payload:   The name-value pairs for the event
             :type  payload:   dict(string:*)
         """
-        self.buffer.append(payload)
+        if self.method == "post":
+            self.buffer.append({key: str(payload[key]) for key in payload})
+        else:
+            self.buffer.append(payload)
 
         if len(self.buffer) >= self.buffer_size:
             return self.flush()
