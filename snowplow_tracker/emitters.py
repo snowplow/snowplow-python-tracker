695387f634dd949cf26834fa25d6b697dbaeda59
Julio Menendez <hello@juliomenendez.com>
2018-03-05 14:55:58 -0800

    Initialize Celery in CeleryEmitter (close #226)

--- snowplow_tracker/emitters.py
+++ snowplow_tracker/emitters.py
@@ -51,16 +51,6 @@ new_contract("function", lambda x: hasattr(x, "__call__"))
 
 new_contract("redis", lambda x: isinstance(x, (redis.Redis, redis.StrictRedis)))
 
-try:
-    # Check whether a custom Celery configuration module named "snowplow_celery_config" exists
-    import snowplow_celery_config
-    app = Celery()
-    app.config_from_object(snowplow_celery_config)
-except ImportError:
-    # Otherwise configure Celery with default settings
-    snowplow_celery_config = None
-    app = Celery("Snowplow", broker="redis://guest@localhost//")
-
 
 class Emitter(object):
     """
@@ -385,29 +375,37 @@ class AsyncEmitter(Emitter):
             self.queue.task_done()
 
 
-@app.task(bind=True, name='tasks.flush')  # the self passed with bind can be used for on_fail/retrying
-def flush_emitter(self, emitter):
-    try:
-        emitter.flush()
-    finally:
-        logger.info("Flush called on emitter")
-
-
 class CeleryEmitter(Emitter):
     """
         Uses a Celery worker to send HTTP requests asynchronously.
         Works like the base Emitter class,
         but on_success and on_failure callbacks cannot be set.
     """
+    celery_app = None
+
     def __init__(self, endpoint, protocol="http", port=None, method="get", buffer_size=None, byte_limit=None):
         super(CeleryEmitter, self).__init__(endpoint, protocol, port, method, buffer_size, None, None, byte_limit)
+
+        try:
+            # Check whether a custom Celery configuration module named "snowplow_celery_config" exists
+            import snowplow_celery_config
+            self.celery_app = Celery()
+            self.celery_app.config_from_object(snowplow_celery_config)
+        except ImportError:
+            # Otherwise configure Celery with default settings
+            self.celery_app = Celery("Snowplow", broker="redis://guest@localhost//")
+
+        self.async_flush = self.celery_app.task(self.async_flush)
 
     def flush(self):
         """
             Schedules a flush task
         """
-        flush_emitter.delay(self)  # passes emitter (self - CeleryEmitter) to task
+        self.async_flush.delay()
         logger.info("Scheduled a Celery task to flush the event queue")
+
+    def async_flush(self):
+        super(CeleryEmitter, self).flush()
 
 
 class RedisEmitter(object):
