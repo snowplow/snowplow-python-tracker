c7ab764a3ca12aba1d7a74c74725a774eee57d11
Paul Boocock <paul@snowplowanalytics.com>
2020-10-10 12:21:17 +0100

    Fix incompatible versions of greenlet and gevent (closes #236)

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     ],
 
     install_requires=[
-        "greenlet>=0.4.10,<1.0",
+        "greenlet>=0.4.10,<=0.4.16",
         "requests>=2.2.1,<3.0",
         "pycontracts>=1.7.6,<2.0",
         "celery>=4.0,<5.0",
