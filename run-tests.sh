163922f35e563485af9b179bde135527391900bb
Paul Boocock <paul@snowplowanalytics.com>
2020-10-10 12:21:45 +0100

    Update build to Active Python Releases (closes #237)

--- run-tests.sh
+++ run-tests.sh
@@ -17,35 +17,53 @@ eval "$(pyenv virtualenv-init -)"
 function deploy {
   # pyenv install 2.7.15
   if [ ! -f ~/.pyenv/versions/tracker27 ]; then
-    pyenv virtualenv 2.7.15 tracker27
+    pyenv virtualenv 2.7.18 tracker27
     pyenv activate tracker27
     pip install .
     pip install -r requirements-test.txt
     source deactivate
   fi
 
-  # pyenv install 3.4.9
-  if [ ! -f ~/.pyenv/versions/tracker34 ]; then
-    pyenv virtualenv 3.4.9 tracker34
-    pyenv activate tracker34
-    pip install .
-    pip install -r requirements-test.txt
-    source deactivate
-  fi
-
-  # pyenv install 3.5.2
+  # pyenv install 3.5.10
   if [ ! -f ~/.pyenv/versions/tracker35 ]; then
-    pyenv virtualenv 3.5.2 tracker35
+    pyenv virtualenv 3.5.10 tracker35
     pyenv activate tracker35
     pip install .
     pip install -r requirements-test.txt
     source deactivate
   fi
 
-  # pyenv install 3.7.1
+  # pyenv install 3.6.12
+  if [ ! -f ~/.pyenv/versions/tracker36 ]; then
+    pyenv virtualenv 3.6.12 tracker36
+    pyenv activate tracker36
+    pip install .
+    pip install -r requirements-test.txt
+    source deactivate
+  fi
+
+  # pyenv install 3.7.9
   if [ ! -f ~/.pyenv/versions/tracker37 ]; then
-    pyenv virtualenv 3.7.1 tracker37
+    pyenv virtualenv 3.7.9 tracker37
     pyenv activate tracker37
+    pip install .
+    pip install -r requirements-test.txt
+    source deactivate
+  fi
+
+  # pyenv install 3.8.6
+  if [ ! -f ~/.pyenv/versions/tracker38 ]; then
+    pyenv virtualenv 3.8.6 tracker38
+    pyenv activate tracker38
+    pip install .
+    pip install -r requirements-test.txt
+    source deactivate
+  fi
+
+  # pyenv install 3.9.0
+  if [ ! -f ~/.pyenv/versions/tracker39 ]; then
+    pyenv virtualenv 3.9.0 tracker39
+    pyenv activate tracker39
     pip install .
     pip install -r requirements-test.txt
     source deactivate
@@ -58,24 +76,34 @@ function run_tests {
   pytest -s
   source deactivate
   
-  pyenv activate tracker34
+  pyenv activate tracker35
   pytest
   source deactivate
-  
-  pyenv activate tracker35
+
+  pyenv activate tracker36
   pytest
   source deactivate
 
   pyenv activate tracker37
   pytest
-  source deactivate 
+  source deactivate
+
+  pyenv activate tracker38
+  pytest
+  source deactivate
+
+  pyenv activate tracker39
+  pytest
+  source deactivate
 }
 
 function refresh_deploy {
   pyenv uninstall -f tracker27
-  pyenv uninstall -f tracker34
   pyenv uninstall -f tracker35
+  pyenv uninstall -f tracker36
   pyenv uninstall -f tracker37
+  pyenv uninstall -f tracker38
+  pyenv uninstall -f tracker39
 }
 
 
