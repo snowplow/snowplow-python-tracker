422dd6583d088d40f26d1ca599feed7a875f0b63
Jack Keene <jack.keene@snowplowanalytics.com>
2022-07-18 15:44:06 +0100

    Add Sphinx workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))	
-print("#####==========================================###############")
-print(sys.executable)
-print("#####==========================================###############")
 
 # -- Project information -----------------------------------------------------
 
