a5d9d1b833c86dac1f916254e20e3223634c7d98
Jack Keene <jack.keene@snowplowanalytics.com>
2022-07-20 14:23:24 +0100

    Change Sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,12 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))	
+
+
 
 # -- Project information -----------------------------------------------------
 
@@ -31,6 +37,8 @@ release = '0.11'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme =     'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
