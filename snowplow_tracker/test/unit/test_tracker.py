492ee1b25c01e6f5609b7a889c3e625f3007c525
Jack-Keene <jack.keene@snowplow.io>
2023-01-19 14:58:58 +0000

    Update unit tests

--- snowplow_tracker/test/unit/test_tracker.py
+++ snowplow_tracker/test/unit/test_tracker.py
@@ -44,7 +44,7 @@ REMOVE_FROM_CART_SCHEMA = (
 FORM_CHANGE_SCHEMA = "iglu:com.snowplowanalytics.snowplow/change_form/jsonschema/1-0-0"
 FORM_SUBMIT_SCHEMA = "iglu:com.snowplowanalytics.snowplow/submit_form/jsonschema/1-0-0"
 SITE_SEARCH_SCHEMA = "iglu:com.snowplowanalytics.snowplow/site_search/jsonschema/1-0-0"
-SCREEN_VIEW_SCHEMA = "iglu:com.snowplowanalytics.mobile/screen_view/jsonschema/1-0-0"
+SCREEN_VIEW_SCHEMA = "iglu:com.snowplowanalytics.snowplow/screen_view/jsonschema/1-0-0"
 
 # helpers
 _TEST_UUID = "5628c4c6-3f8a-43f8-a09f-6ff68f68dfb6"
@@ -1291,6 +1291,32 @@ class TestTracker(unittest.TestCase):
         self.assertTrue(callArgs[2] is None)
 
     @mock.patch("snowplow_tracker.Tracker.track_self_describing_event")
+    def test_track_mobile_screen_view(self, mok_track_unstruct: Any) -> None:
+        mokEmitter = self.create_patch("snowplow_tracker.Emitter")
+        e = mokEmitter()
+
+        mok_track_unstruct.side_effect = mocked_track_unstruct
+
+        t = Tracker(e)
+        ctx = SelfDescribingJson("test.context.schema", {"user": "tester"})
+        evTstamp = 1399021242030
+
+        t.track_mobile_screen_view(
+            "screenId", "screenName", context=[ctx], tstamp=evTstamp
+        )
+
+        expected = {
+            "schema": MOBILE_SCREEN_VIEW_SCHEMA,
+            "data": {"name": "screenName", "id": "screenId"},
+        }
+
+        callArgs = mok_track_unstruct.call_args_list[0][0]
+        self.assertEqual(len(callArgs), 4)
+        self.assertDictEqual(callArgs[0].to_json(), expected)
+        self.assertIs(callArgs[1][0], ctx)
+        self.assertEqual(callArgs[2], evTstamp)
+
+    @mock.patch("snowplow_tracker.Tracker.track_self_describing_event")
     def test_track_screen_view(self, mok_track_unstruct: Any) -> None:
         mokEmitter = self.create_patch("snowplow_tracker.Emitter")
         e = mokEmitter()
@@ -1301,7 +1327,7 @@ class TestTracker(unittest.TestCase):
         ctx = SelfDescribingJson("test.context.schema", {"user": "tester"})
         evTstamp = 1399021242030
 
-        t.track_screen_view("screenId", "screenName", context=[ctx], tstamp=evTstamp)
+        t.track_screen_view("screenName", "screenId", context=[ctx], tstamp=evTstamp)
 
         expected = {
             "schema": SCREEN_VIEW_SCHEMA,
