2a1c875e21d8201bccc9d7e1fcdbdfc5d63a5509
Jack-Keene <jack.keene@snowplow.io>
2023-05-31 11:09:00 +0100

    Add Events module init

---
+++ snowplow_tracker/events/__init__.py
@@ -0,0 +1,5 @@
+from snowplow_tracker.events.event import Event
+from snowplow_tracker.events.page_ping import PagePing
+from snowplow_tracker.events.pageview import Pageview
+from snowplow_tracker.events.self_describing import SelfDescribing
+from snowplow_tracker.events.struct_event import StructEvent
