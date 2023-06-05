1e19bf081266021d118c375c24d9d240ea5439ef
Jack-Keene <jack.keene@snowplow.io>
2023-06-05 15:02:35 +0100

    Add subject to example app

--- examples/tracker_api_example/app.py
+++ examples/tracker_api_example/app.py
@@ -31,25 +31,38 @@ def main():
 
     print("Sending events to " + e.endpoint)
 
-    page_view = PageView(page_url="https://www.snowplow.io", page_title="Homepage")
+    page_view = PageView(
+        page_url="https://www.snowplow.io",
+        page_title="Homepage",
+        event_subject=t.subject,
+    )
     t.track(page_view)
 
-    page_ping = PagePing(page_url="https://www.snowplow.io", page_title="Homepage")
+    page_ping = PagePing(
+        page_url="https://www.snowplow.io",
+        page_title="Homepage",
+        event_subject=t.subject,
+    )
     t.track(page_ping)
 
     link_click = SelfDescribing(
         SelfDescribingJson(
             "iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1",
             {"targetUrl": "https://www.snowplow.io"},
-        )
+        ),
+        event_subject=t.subject,
     )
     t.track(link_click)
 
-    screen_view = ScreenView(id_="id", name="name")
+    screen_view = ScreenView(id_="id", name="name", event_subject=t.subject)
     t.track(screen_view)
 
     struct_event = StructEvent(
-        category="shop", action="add-to-basket", property_="pcs", value=2
+        category="shop",
+        action="add-to-basket",
+        property_="pcs",
+        value=2,
+        event_subject=t.subject,
     )
     t.track(struct_event)
     t.flush()
