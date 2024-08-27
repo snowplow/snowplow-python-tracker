ba24c288ffb2837c1e5224fe7edf7dfe86e75584
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2024-08-27 11:42:28 +0100

    Prepare for 1.0.3 release

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ authors_email_str = ", ".join(authors_email_list)
 
 setup(
     name="snowplow-tracker",
-    version="1.0.2",
+    version="1.0.3",
     author=authors_str,
     author_email=authors_email_str,
     packages=["snowplow_tracker", "snowplow_tracker.test", "snowplow_tracker.events"],
